7db34793d79b5f366419086eb8666b1b2f8f1a04
sumathiD <sumathi@sdtech.io>
2022-09-26 15:07:53 +0530

    Header and Sider menu ui updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,14 +1,20 @@
 import 'antd/dist/antd.css';
 import { Menu } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
+import preferredPayment from "../assets/images/preferred-payment.svg";
+import creditDebit from "../assets/images/credit-debit.svg";
+import wallets from "../assets/images/wallets.svg";
+import upi from "../assets/images/upi.svg";
+import netBanking from "../assets/images/net-banking.svg";
+import others from "../assets/images/others.svg";
 
 import { useNavigate } from "react-router-dom";
 import RoutePage from './routePage';
 const Dashboard = () => {
     const navigate = useNavigate()
     return (
-        <div style={{ display: "flex", flexDirection: "row", height: "100vh", marginLeft: "200px", }}>
-            <Menu className='bg'
+        <div style={{ display: "flex", flexDirection: "row", height: "100vh", marginLeft: "251px", }} className="Sider__Payment__Menu">
+            <Menu className='bg' style={{background: '#040748', paddingTop: '31px'}}
                 onClick={({ key }) => {
                     if (key === "others") {
                         //todo
@@ -17,12 +23,12 @@ const Dashboard = () => {
                     }
                 }}
                 items={[
-                    { label: 'preferred payment', key: "/", icon: <HomeOutlined /> },
-                    { label: 'creditdebitcards', key: "/Creditpage", icon: <HomeOutlined /> },
-                    { label: 'wallets', key: "Walletspage", icon: <HomeOutlined /> },
-                    { label: 'UPI', key: "Upipage", icon: <HomeOutlined /> },
-                    { label: 'net banking', key: "Netbankingpage", icon: <HomeOutlined /> },
-                    { label: 'others', key: "Others", icon: <HomeOutlined /> },
+                    { label: 'Preferred Payment', key: "/", icon: <img src={preferredPayment} /> },
+                    { label: 'Credit & Debit Cards', key: "/Creditpage", icon: <img src={creditDebit} /> },
+                    { label: 'Wallets', key: "Walletspage", icon: <img src={wallets} /> },
+                    { label: 'UPI', key: "Upipage", icon: <img src={upi} /> },
+                    { label: 'Net Banking ', key: "Netbankingpage", icon: <img src={netBanking} /> },
+                    { label: 'Others', key: "Others", icon: <img src={others} /> },
                 ]}>
 
             </Menu>
--- src/components/headerPage.tsx
+++ src/components/headerPage.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import logo from "../assets/images/star-logo.svg";
+import arrowR from "../assets/images/arrow-r.svg";
+
 
 const HeaderPage = () => {
     return (
         <div className='header' style={{
-            background: "rgba(167, 29, 150, 0.136)",
-            height: "39px"
+            background: "#fff",
+            height: "68px",
+            display: 'flex',
+            boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)',
+            marginBottom: '50px',
         }}>
-            <p style={{ marginLeft: "200px", paddingTop: "10px" }}> payment{">"}Credit & Debit card</p>
+            <div style={{
+                padding: 13,
+                boxSizing: 'border-box',
+                width: '219px',
+            }}>
+                <img src={logo} alt="Star" />
+            </div>
+            <div style={{
+                boxSizing: 'border-box',
+               paddingTop: '10px',
+            }}>
+            <p style={{ 
+                paddingTop: "10px",
+                color: '#757688',
+                fontSize: '17px',
+            }}> 
+            Payment  <span style={{ padding: '0 5px' }}><img src={arrowR} alt="" />  </span>
+            <span style={{
+                color: '#040748',
+                fontWeight:'500',
+            }}>Credit &amp; Debit card</span></p>
+            </div>
+            
         </div>
     )
 }
