40f91bc799e786748086c810a49ee0b458522589
sumathiD <sumathi@sdtech.io>
2022-09-27 11:53:18 +0530

    visa, master images added

--- src/components/creditPage.tsx
+++ src/components/creditPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Card, Col, Input, Row, Form } from 'antd';
 import { Formik } from 'formik';
+import visaimg from "../assets/images/visa-img.png";
+import masterimg from "../assets/images/master-img.png";
+
 const CreditPage = () => {
   return (
     <div className="site-card-border-less-wrapper">
@@ -16,14 +19,16 @@ const CreditPage = () => {
               <div className="inputBox">
                 <label>Credit/Debit Card Number<span>*</span></label><br />
                 <input type="text" className="inputBox creditwidth" />
+                <img src={visaimg} alt="Visa" style={{ width: '60px', height: '40px', marginLeft: "20px" }} />
+                <img src={masterimg} alt="Master" style={{ width: '60px', height: '40px', marginLeft: "5px" }} />
                 {/* <span className="img"> */}
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMvKRme09fZczEI27dK0SboCY_I6-9Ndkaww&usqp=CAU"
+                {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMvKRme09fZczEI27dK0SboCY_I6-9Ndkaww&usqp=CAU"
                   style={{ width: '40px', height: '40px', marginLeft: "20px", background: "none" }}
                 />
 
                 <img src="https://banner2.cleanpng.com/20180403/qte/kisspng-mastercard-credit-card-visa-payment-service-mastercard-5ac3fae6d9ece0.7626666215227931908926.jpg"
                   style={{ width: '60px', height: '40px' }}
-                />
+                /> */}
                 {/* </span> */}
               </div>
               <div className="row">
